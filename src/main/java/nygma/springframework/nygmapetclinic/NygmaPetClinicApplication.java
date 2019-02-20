fb07dd0159af4940ef5a9f7743b30ccb56cf5dc9
Ninad Thakare <ninad.thakare@neosofttech.com>
2019-02-20 12:13:06 +0530

    Initial Project Adjust

---
+++ src/main/java/nygma/springframework/nygmapetclinic/NygmaPetClinicApplication.java
@@ -0,0 +1,13 @@
+package nygma.springframework.nygmapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NygmaPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NygmaPetClinicApplication.class, args);
+    }
+
+}
