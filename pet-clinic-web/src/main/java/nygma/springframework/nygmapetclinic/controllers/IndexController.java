cd368f2d447a53b0043f2987e42668b77d816ccb
Ninad Thakare <ninad.thakare@neosofttech.com>
2019-03-08 12:56:39 +0530

    Added Index Page Closes #7

---
+++ pet-clinic-web/src/main/java/nygma/springframework/nygmapetclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package nygma.springframework.nygmapetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String loadIndex() {
+        return "index";
+    }
+}
