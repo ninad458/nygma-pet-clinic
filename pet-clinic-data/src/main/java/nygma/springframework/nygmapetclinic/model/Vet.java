5672b1e67c2dc23adf2e0fa0b3c4ef8f12305169
Ninad Thakare <ninad.thakare@neosofttech.com>
2019-03-11 19:44:04 +0530

    Created Speciality and added them to Vet Closes #24

--- pet-clinic-data/src/main/java/nygma/springframework/nygmapetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/nygma/springframework/nygmapetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package nygma.springframework.nygmapetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
