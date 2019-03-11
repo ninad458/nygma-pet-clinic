0fc2ccddb2950969339881e7050bc63bc54d6fdd
Ninad Thakare <ninad.thakare@neosofttech.com>
2019-03-11 11:55:22 +0530

    List all Owners on the Owner Index Page Closes #13

--- pet-clinic-web/src/main/java/nygma/springframework/nygmapetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/nygma/springframework/nygmapetclinic/controllers/OwnersController.java
@@ -1,14 +1,23 @@
 package nygma.springframework.nygmapetclinic.controllers;
 
+import nygma.springframework.nygmapetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOfOwners() {
+    public String listOfOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
