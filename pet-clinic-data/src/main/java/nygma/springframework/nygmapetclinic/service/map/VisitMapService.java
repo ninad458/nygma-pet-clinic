ded1a700cd28d3f5f0c7a1a8ed0e958144bb2821
Ninad Thakare <ninad.thakare@neosofttech.com>
2019-03-13 19:00:10 +0530

    Created Map Service for Visit Closes #42

---
+++ pet-clinic-data/src/main/java/nygma/springframework/nygmapetclinic/service/map/VisitMapService.java
@@ -0,0 +1,19 @@
+package nygma.springframework.nygmapetclinic.service.map;
+
+import nygma.springframework.nygmapetclinic.model.Visit;
+import nygma.springframework.nygmapetclinic.service.VisitService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitMapService extends GenericBaseEntityMap<Visit> implements VisitService {
+
+    @Override
+    public Visit save(Visit object) {
+        Visit visit = super.save(object);
+        if (visit.getPet() == null || visit.getPet().getId() == null || visit.getPet().getOwner() == null ||
+                visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return visit;
+    }
+}
