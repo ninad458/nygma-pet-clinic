9d8d348a6f27cf13d048902fb598b8aac24988bd
Ninad Thakare <ninad.thakare@neosofttech.com>
2019-03-13 19:00:10 +0530

    Created Map Service for Visit Closes #42

---
+++ pet-clinic-data/src/main/java/nygma/springframework/nygmapetclinic/service/VisitService.java
@@ -0,0 +1,6 @@
+package nygma.springframework.nygmapetclinic.service;
+
+import nygma.springframework.nygmapetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
