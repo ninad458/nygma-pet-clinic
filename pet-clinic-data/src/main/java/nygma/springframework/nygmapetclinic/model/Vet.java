bf6ca66d0833f0401d5f7be0f09f16a56dbdf69a
Ninad Thakare <ninad.thakare@neosofttech.com>
2019-03-14 11:27:15 +0530

    Added Project Lombok Closes #48

--- pet-clinic-data/src/main/java/nygma/springframework/nygmapetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/nygma/springframework/nygmapetclinic/model/Vet.java
@@ -1,10 +1,17 @@
 package nygma.springframework.nygmapetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "vets")
 public class Vet extends Person {
 
@@ -13,11 +20,4 @@ public class Vet extends Person {
             inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
