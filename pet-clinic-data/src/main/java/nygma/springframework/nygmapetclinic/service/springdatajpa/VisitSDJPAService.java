7b347ccd323aeb129746afb6d4f5f36d2972f470
Ninad Thakare <ninad.thakare@neosofttech.com>
2019-03-14 09:41:51 +0530

    Created JPA Service for Visit Closes #43

---
+++ pet-clinic-data/src/main/java/nygma/springframework/nygmapetclinic/service/springdatajpa/VisitSDJPAService.java
@@ -0,0 +1,48 @@
+package nygma.springframework.nygmapetclinic.service.springdatajpa;
+
+import nygma.springframework.nygmapetclinic.model.Visit;
+import nygma.springframework.nygmapetclinic.repositories.VisitRepository;
+import nygma.springframework.nygmapetclinic.service.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJPAService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJPAService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
