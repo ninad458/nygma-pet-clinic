b0389f2ea50e378a8ddc12f41d629dfc4a4c99fe
Ninad Thakare <ninad.thakare@neosofttech.com>
2019-03-22 16:39:39 +0530

    Added Visit Controller and mappings Closes #61

---
+++ pet-clinic-web/src/main/java/nygma/springframework/nygmapetclinic/controllers/VisitController.java
@@ -0,0 +1,57 @@
+package nygma.springframework.nygmapetclinic.controllers;
+
+import nygma.springframework.nygmapetclinic.model.Pet;
+import nygma.springframework.nygmapetclinic.model.Visit;
+import nygma.springframework.nygmapetclinic.service.PetService;
+import nygma.springframework.nygmapetclinic.service.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class VisitController {
+
+    private final VisitService visitService;
+    private final PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable("petId") Long petId, Model model) {
+        Pet pet = petService.findById(petId);
+        model.addAttribute("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before initNewVisitForm is called
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable("petId") Long petId, Model model) {
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before processNewVisitForm is called
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pets/createOrUpdateVisitForm";
+        } else {
+            visitService.save(visit);
+            return "redirect:/owners/{ownerId}";
+        }
+    }
+}
